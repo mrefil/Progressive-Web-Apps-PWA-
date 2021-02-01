8b2537e9ccf86488d452a34a8519e0f1255d95b7
mrefil <muefiloglu@protonmail.com>
2021-02-01 04:40:20 +0300

    fetch sw.js

--- public/sw.js
+++ public/sw.js
@@ -7,3 +7,8 @@ self.addEventListener('activate', function(event) {
     console.log('[Service Worker] Activating Service Worker...', event);
     return self.ClientRectList.claim();
 });
+
+self.addEventListener('fetch', function(event) {
+    console.log('[Service Worker] Fetching Service Worker...', event);
+    event.respondWith(fetch(event.request));
+});
